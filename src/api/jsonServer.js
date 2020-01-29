f5909ebaa49bbe16f93af03cd70dd31f78a320d3
Maxmilián Fedorko <maxmilian.fedorko@evalue.cz>
2020-01-29 14:30:57 +0100

    Some additions to Dashboards Ccy displaying

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 
 const instance = axios.create({
-  baseURL: "http://f09ef294.ngrok.io"
+  baseURL: "http://6bf1a6e2.ngrok.io"
 });
 
 instance.interceptors.request.use(
--- src/screens/DashBoard.js
+++ src/screens/DashBoard.js
@@ -1,32 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Flatlist, StatusBar, FlatList } from "react-native";
-import { Text, Button } from "react-native-elements";
+import { Text, Button, SearchBar } from "react-native-elements";
 import EurIcon from "react-native-vector-icons/FontAwesome";
 
 import Charts from "../components/charts/Charts";
 
 const DashBoard = ({ navigation }) => {
-  const [currencyData, setData] = useState();
+  const [currencyData, setData] = useState([]);
+
   useEffect(() => {
     (async function courseFetchFunction() {
       const response = await fetch(
         "http://data.fixer.io/api/latest?access_key=a2be8773d06f1fd33b5714f7bedd371a"
       );
       const data = await response.json();
+
+      const arrayData = Object.entries(data.rates);
+
+      console.log(arrayData);
+
+      setData(arrayData);
+
+      // Object.keys(arrayData).forEach(key => {
+      //   console.log(key, arrayData[key]);
+      // });
+
       console.log(
         `Status: ${data.success}, Date:${data.date}, Currency: ${data.base}`
       );
-      setData(data);
     })();
   }, []);
+
   return (
     <View
       style={{ flex: 1, backgroundColor: "aliceblue", alignItems: "center" }}
     >
-      <View style={styles.userIconView}>
+      <View style={styles.dashboardIconView}>
         <EurIcon name="euro" color="dodgerblue" size={80} />
       </View>
-      <View style={styles.userInfoView}>
+      <View style={styles.dashboardInfoView}>
+        <SearchBar
+          showCancel="true"
+          placeholder="Search for exchange rate"
+          lightTheme="true"
+          round="true"
+        />
+        <FlatList
+          data={currencyData}
+          renderItem={({ item }) => (
+            <Button
+              style={{ marginTop: 10 }}
+              onPress={() => {}}
+              title={item}
+              type="outline"
+            ></Button>
+          )}
+        ></FlatList>
+      </View>
+      {/* <View style={styles.userInfoView}>
         <Button
           onPress={() => {}}
           title={`CZK: ${currencyData?.rates.CZK}`}
@@ -44,7 +75,7 @@ const DashBoard = ({ navigation }) => {
           type="outline"
           style={{ marginTop: 10 }}
         ></Button>
-      </View>
+      </View> */}
     </View>
   );
 };
@@ -56,13 +87,13 @@ DashBoard.navigationOptions = () => {
 };
 
 const styles = StyleSheet.create({
-  userIconView: {
+  dashboardIconView: {
     alignItems: "center",
     width: "100%",
     height: 80,
     backgroundColor: "white"
   },
-  userInfoView: {
+  dashboardInfoView: {
     width: "90%%",
     height: "80%",
     backgroundColor: "white",
